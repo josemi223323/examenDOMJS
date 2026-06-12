e462ed9f59a93a0517f564af805e0bfdae430fd0
josemi <you@example.com>
2026-06-13 00:38:55 +0200

    examen mejorado

--- script/funcionesDOM.js
+++ script/funcionesDOM.js
@@ -5,35 +5,43 @@ function cambiarColor() {
   console.log(colorTitulo);
   if (contador < 4) {
     if (colorTitulo == "rgb(0, 139, 139)") {
-      titulo.style.color = "orangered";
+      titulo.classList.remove("cyan");
+      titulo.classList.add("naranja");
     } else if (colorTitulo == "rgb(255, 69, 0)") {
-      titulo.style.color = "darkcyan";
+      titulo.classList.remove("naranja");
+      titulo.classList.add("cyan");
     }
     contador++;
   } else {
-    titulo.style.color = "green";
+    titulo.classList.remove("naranja");
+    titulo.classList.add("verde");
     titulo.removeAttribute("onclick");
   }
 }
-const matrizImagenes = [
-  ["../img/refrig1.jpg", "../img/refrig2.jpg", "../img/refrig3.jpg"],
-  ["../img/micro1.jpg", "../img/micro2.jpg", "../img/micro3.jpg"],
-  ["../img/graf1.jpg", "../img/graf2.jpg", "../img/graf3.jpg"],
-  ["../img/placa1.jpg", "../img/placa2.jpg", "../img/placa3.jpg"],
-];
+let matrizImagenes;
+function cargarContenido() {
+  const matrizImagenes = [
+    ["../img/refrig1.jpg", "../img/refrig2.jpg", "../img/refrig3.jpg"],
+    ["../img/micro1.jpg", "../img/micro2.jpg", "../img/micro3.jpg"],
+    ["../img/graf1.jpg", "../img/graf2.jpg", "../img/graf3.jpg"],
+    ["../img/placa1.jpg", "../img/placa2.jpg", "../img/placa3.jpg"],
+  ];
 
-let nuevoSeccion = document.createElement("section");
-let nuevoH2 = document.createElement("h2");
-nuevoH2.innerText = "componentes";
-document.body.appendChild(nuevoSeccion);
-nuevoSeccion.appendChild(nuevoH2);
+  let nuevoSeccion = document.createElement("section");
+  let nuevoH2 = document.createElement("h2");
+  nuevoH2.innerText = "componentes";
+  document.body.appendChild(nuevoSeccion);
+  nuevoSeccion.appendChild(nuevoH2);
 
-let nuevoDiv = document.createElement("div");
-nuevoSeccion.appendChild(nuevoDiv);
-
-let select = document.getElementsByTagName("select")[0];
-function selectOpciones() {
-  nuevoDiv.innerHTML = ""; 
+  let nuevoDiv = document.createElement("div");
+  nuevoDiv.id = "componentes"
+  nuevoSeccion.appendChild(nuevoDiv);
+  return matrizImagenes;
+}
+function selectOpciones(matrizImagenes) {
+  let nuevoDiv = document.getElementById("componentes");
+  let select = document.getElementsByTagName("select")[0];
+  nuevoDiv.innerHTML = "";
 
   let indice = parseInt(select.value);
 
@@ -43,10 +51,8 @@ function selectOpciones() {
     nuevoDiv.appendChild(nuevaImagen);
   }
 }
-selectOpciones(); 
-select.addEventListener("change", selectOpciones);
 function mostrarCorreo(evento) {
-    evento.preventDefault();
-    const email = document.getElementById('email').value;
-    alert("tu correo es " + email);
+  evento.preventDefault();
+  const email = document.getElementById('email').value;
+  alert("tu correo es " + email);
 }
